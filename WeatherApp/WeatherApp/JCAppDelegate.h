9a55ee050666eaca47dcde32f553e0b1da65c00b
jpcarreira <jpmavcarreira@gmail.com>
2014-03-19 22:47:57 +0000

    added empty xcode project (empty application)

---
+++ WeatherApp/WeatherApp/JCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JCAppDelegate.h
+//  WeatherApp
+//
+//  Created by João Carreira on 19/03/14.
+//  Copyright (c) 2014 João Carreira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WeatherApp/WeatherApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WeatherApp
+//
+//  Created by João Carreira on 19/03/14.
+//  Copyright (c) 2014 João Carreira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JCAppDelegate class]));
+    }
+}
